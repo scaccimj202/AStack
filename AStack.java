3ff3a45d5f000cd0300d78f978d8d9e0461e5b55
Matt Scaccia <scaccimj202@potsdam.edu>
2020-03-29 13:18:26 -0400

    Fixed typo in flower box

--- AStack.java
+++ AStack.java
@@ -3,7 +3,7 @@
  *course CIS303
  *@since 3/29/2020
  *@assignment 4b
- description: AStack data structure adapted from Clifford Shaffers Data Structures and Algorithim Analysis, Chapter 4.2.1
+ *description: AStack data structure adapted from Clifford Shaffers Data Structures and Algorithim Analysis, Chapter 4.2.1
  */
 
  public class AStack<E> implements StackADT<E>{
